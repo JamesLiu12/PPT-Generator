1dc074fbb5fca539a713324225263ad78790eafb
JamesLiu12 <39219648+JamesLiu12@users.noreply.github.com>
2023-10-04 23:09:32 +0800

    Finish text upload

---
+++ src/lib/Button.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+
+    import {createEventDispatcher} from "svelte";
+
+    const dispatch = createEventDispatcher();
+
+    function onclick() : void {
+        dispatch("click");
+    }
+</script>
+
+<div class="button" on:click={onclick}>
+    <slot/>
+</div>
+
+<style lang="scss">
+    @import "../global";
+
+    .button {
+        @extend %no-sel;
+        cursor: pointer;
+
+        width: fit-content;
+        border: 1px solid gray; border-radius: 0.3rem;
+        padding: 1rem 1.4rem;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+    import "../global.scss";
+    import Button from "$lib/Button.svelte";
+
+    const title = "Teaching Material Generator";
+
+    let teachingMaterial: HTMLElement;
+    let textsToUpload: Array<string> = [];
+
+    function upload() {
+        textsToUpload.push(teachingMaterial.innerHTML);
+        console.log("Upload: ", textsToUpload);
+    }
+
+    function generateOutline() {
+        console.log("Generate");
+    }
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+
+<nav>{title}</nav>
+
+<div id="wrapper">
+    <div id="teaching-material" contenteditable="true" bind:this={teachingMaterial}/>
+
+    <br>
+
+    <div id="operations">
+        <Button on:click={upload}>
+            Upload
+        </Button>
+        <Button on:click={generateOutline}>
+            Generate outline
+        </Button>
+    </div>
+</div>
+
+
+<style lang="scss">
+    nav {
+        padding: 1rem 2rem;
+        background: #eee;
+    }
+    #wrapper {
+        max-width: 40rem; width: 100%;
+        margin: 0 auto;
+    }
+    #teaching-material {
+        height: 20rem;
+        border: 1px solid gray;
+        padding: 1rem 1.5rem;
+
+        overflow-y: auto;
+    }
+
+    #operations {
+        display: flex;
+        justify-content: space-around;
+    }
+</style>
